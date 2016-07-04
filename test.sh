a7f5f41d2e19a6552d372625e4b73ae5ab3abcb3
Joshua Charles Campbell <joshua2@ualberta.ca>
2016-07-04 13:33:16 -0600

    fix test script due to git tag not showing tags in order

--- test.sh
+++ test.sh
@@ -15,16 +15,19 @@ git commit -m 'fun.py init'
 echo "# modification" >> fun.py
 # This is supposed to not be successful
 ./cbpy fun.py
-git tag | head -n 1 | fgrep failure || (echo "Did not commit a failure"; exit)
+git log --date-order --tags --simplify-by-decoration --pretty=format:"%ci %d" \
+ | head -n 1 | fgrep failure || (echo "Did not commit a failure"; exit)
 echo "Marking a syntax error"
 ./cbpy -s # mark syntax error
-git tag | tail -n 1 | fgrep syntax || (echo "Did not commit a failure"; exit)
+git log --date-order --tags --simplify-by-decoration --pretty=format:"%ci %d" \
+ | head -n 1 | fgrep syntax || (echo "Did not commit a failure"; exit)
 cat <<EOF > fun.py
 import sys
 print("SUCCESS SUCCESS")
 EOF
 ./cbpy fun.py
-git tag | tail -n 1 | fgrep success || (echo "Did not commit a success"; exit)
+git log --date-order --tags --simplify-by-decoration --pretty=format:"%ci %d" \
+  | head -n 1 | fgrep -i success || (echo "Did not commit a success"; exit)
 echo "Press Ctrl-C right now to inspect the test directory on your own $DIR"
 read
 rm -rf $DIR
