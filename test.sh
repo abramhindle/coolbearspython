21c4f0f3738cc45d26b915e7ef9b6d86fd095226
Abram Hindle <abram.hindle@ualberta.ca>
2016-07-04 12:02:53 -0600

    success and failure pass

--- test.sh
+++ test.sh
@@ -13,4 +13,12 @@ EOF
 git add fun.py
 git commit -m 'fun.py init'
 echo "# modification" >> fun.py
+# This is supposed to not be successful
 ./cbpy fun.py
+git tag | head -n 1 | fgrep failure || (echo "Did not commit a failure"; exit)
+cat <<EOF > fun.py
+import sys
+print("SUCCESS SUCCESS")
+EOF
+./cbpy fun.py
+git tag | tail -n 1 | fgrep succs || (echo "Did not commit a success"; exit)
